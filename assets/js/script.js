09db5b5acce55f3a93ad39986e995bac6b0d429f
JosephRule <josephrule1@gmail.com>
2022-02-23 10:41:01 -0800

    better date entry and conditional date formatting using moment library

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -91,9 +93,18 @@ $(".list-group").on("click", "span", function() {
     .val(date);
 
   $(this).replaceWith(dateInput);
+
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  }),
+
+  dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this)
     .val()
     .trim();
@@ -113,8 +124,10 @@ $(".list-group").on("blur", "input[type='text']", function() {
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(date);
-
   $(this).replaceWith(taskSpan);
+
+  // pass task's <li> element into AuditTask to check new due date 
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -219,7 +232,30 @@ $("#trash").droppable({
   }
 });
 
-// load tasks for the first time
+$("#modalDueDate").datepicker({
+  minDate: 1,
+});
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time,"days")) <=2 ) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+
 loadTasks();
 
 
